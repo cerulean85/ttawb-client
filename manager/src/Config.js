6b328c986b7c5092754dafa555c2a24de86cca5c
cerulean85 <zhwan85@gmail.com>
2021-10-21 16:10:15 +0900

    Revert "ttawb-client First upload"
    This reverts commit 61b06b95bf76bb7bb42df9679a1636572772bf67.

--- manager/src/Config.js
+++
@@ -1,2 +0,0 @@
-export const proxyIP = "218.150.182.180"
-export const proxyPort = "3001"
--- manager/src/setProxy.js
+++
@@ -1,9 +0,0 @@
-const proxy = require("http-proxy-middleware");
-
-module.exports = function (app) {
-    app.use(
-        proxy("/api", {
-            tartget: "http://218.150.182.180:3002/"
-        })
-    );
-};
